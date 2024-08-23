b4e6ffa4de30e526491ddf680f7f8911a395bf20
Constance RICHARD <crichard@prestaconcept.net>
2024-08-23 15:14:36 +0200

    Convert yaml translations to xliff

--- tests/Fixtures/Services.php
+++ tests/Fixtures/Services.php
@@ -2,13 +2,13 @@
 
 namespace Presta\MailReceiverBundle\Tests\Fixtures;
 
-use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Component\Translation\Loader\XliffFileLoader;
 use Symfony\Component\Translation\Translator;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class Services
 {
-    private const TRANSLATION_CATALOG = __DIR__ . '/../../translations/PrestaMailReceiverBundle.fr.yaml';
+    private const TRANSLATION_CATALOG = __DIR__ . '/../../translations/PrestaMailReceiverBundle.fr.xlf';
 
     private static $services = [];
 
@@ -16,8 +16,8 @@ final class Services
     {
         if (!isset(self::$services[__FUNCTION__])) {
             self::$services[__FUNCTION__] = $translator = new Translator('fr_FR');
-            $translator->addLoader('yaml', new YamlFileLoader());
-            $translator->addResource('yaml', self::TRANSLATION_CATALOG, 'fr_FR', 'PrestaMailReceiverBundle');
+            $translator->addLoader('xliff', new XliffFileLoader());
+            $translator->addResource('xliff', self::TRANSLATION_CATALOG, 'fr_FR', 'PrestaMailReceiverBundle');
         }
 
         return self::$services[__FUNCTION__];
